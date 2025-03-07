483f7c2a58f5d71a4b5aa86ca1317625cdab81f3
Jason <55947509+Jasonndlovu@users.noreply.github.com>
2025-03-07 07:23:21 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myPWA',
-  webDir: 'www'
-};
-
-export default config;
